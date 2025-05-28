d09cb6d625626ca7a3a0dbf16836c153c5a5a319
Angel Puch <angeljonathanpuchhernandez@gmail.com>
2025-05-27 18:38:31 -0600

    Estructura principal

---
+++ src/main/java/com/app/javafxappaerolinea/JavaFXAppAerolinea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.javafxappaerolinea;
+
+/**
+ *
+ * @author Dell
+ */
+public class JavaFXAppAerolinea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
